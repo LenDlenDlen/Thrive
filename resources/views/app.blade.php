96cbdf9d3e0870252b6c1e509a05d1700d8cc472
Joddy0 <143619313+Joddy0@users.noreply.github.com>
2024-10-08 20:38:25 +0700

    Add navbar component

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.master')
+
+@section('content')
     <h1 class="text-3xl font-bold underline">
         Hello world!
     </h1>
-</body>
-</html>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <title>Thrive</title>
+</head>
+
+<body>
+    <x-navbar />
+    @yield('content')
+</body>
+
+</html>
